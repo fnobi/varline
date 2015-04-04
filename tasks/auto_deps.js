9240cccc3469bfceb797a6d94eecf1c4da82c58b
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-04-04 20:30:53 +0900

    クラス分離成功・その他リファクタも

--- tasks/auto_deps.js
+++ tasks/auto_deps.js
@@ -1,94 +1,52 @@
-module.exports = function (grunt) {
-    var taskName        = 'auto_deps',
-        taskDescription = 'resolve js dependency and concat automatically.',
+var path = require('path');
+var async = require('async');
 
-        path = require('path'),
-        async = require('async'),
-        pathToConcated = require('../lib/pathToConcated'),
-        scriptMatchList = require('../lib/scriptMatchList'),
-        bowerPathList = require('../lib/bowerPathList');
-        _ = grunt.util._;
+module.exports = function (grunt) {
+    var Varline = require(__dirname + '/../lib/Varline');
+
+    var taskName = 'auto_deps';
+    var taskDescription = 'resolve js dependency and concat automatically.';
 
     grunt.file.defaultEncoding = 'utf8';
 
     grunt.registerMultiTask(taskName, taskDescription, function () {
-        var target = this.target,
-            config = grunt.config(taskName)[target],
+        var target = this.target;
+        var config = grunt.config(taskName)[target];
+        var scripts = config.scripts || [];
+        var dest = config.dest || 'js';
+        var done = this.async();
 
-            // config parameters
-            loadPath = config.loadPath || ['src/js/*.js', 'src/js/**/*.js'],
-            scripts = config.scripts || [],
-            locate = config.locate || {},
-            alias = config.alias || {},
-            ignoredNames = config.ignore || [],
-            forceddNames = config.forced || [],
-            dest = config.dest || 'js',
-            wrap = config.wrap ? true : false,
+        var varline = new Varline(config);
 
-            done = this.async();
+        varline.loadBowerComponents(function (err) {
+            // bower componentsの取得に失敗しても、候補に追加されないだけなので、
+            // 全体の処理は気にせず進める
 
-        var pathsForName = scriptMatchList({
-            loadPath: loadPath,
-            locate: locate
-        });
-
-        async.series([function (next) {
-            bowerPathList(process.cwd(), function (err, list) {
-                if (err) {
-                    // bower componentsの取得に失敗しても、候補に失敗するだけなので、
-                    // 全体の処理は気にせず進める
-                    return next();
-                }
-
-                for (var i in list) {
-                    pathsForName[i] = list[i];
-                }
-                next();
-            });
-        }, function (next) {
-            scripts.forEach(function (name) {
+            async.each(scripts, function (name, next) {
                 var destPath = path.join(dest, name + '.js');
-
-                // console.log('[script: %s]', name);
-                // console.log(' - dest path: %s', destPath);
-
-                if (!pathsForName[name]) {
-                    console.error('%s is not found on src.', name);
-                    return;
-                }
-
-                var vars = pathToConcated({
-                    scriptName: name,
-                    pathsForName: pathsForName,
-                    ignoredNames: ignoredNames,
-                    alias: alias
+                varline.resolve(name, function (err, result) {
+                    if (err) {
+                        console.error('[error] %s', destPath);
+                        console.error(err.toString());
+                        next();
+                        return;
+                    }
+                    console.log('[success] %s', destPath);
+                    console.log(result.vars);
+                    grunt.file.write(destPath, result.source);
+                    next();
                 });
-                var sources = [];
-
-                forceddNames.forEach(function (n) {
-                    vars.unshift(n);
-                });
-
-                console.log('[write] %s (%s)', destPath, vars.join(', '));
-
-                vars.forEach(function (name) {
-                    sources.push(grunt.file.read(pathsForName[name]));
-                });
-
-                if (wrap) {
-                    // console.log('[wrap]');
-                    sources.unshift('(function () {');
-                    sources.push('})();');
-                }
-
-                grunt.file.write(destPath, sources.join('\n'));
-            });
-            next();
-        }], function (err) {
-            if (err) {
-                throw new Error(err);
-            }
-            done();
+            }, done);            
         });
     });
 };
+
+
+
+
+
+
+
+
+
+
